414dda6261455c68414f7eb18f2ed459ff6b6ac9
falkesmoon <falkesmoon@gmail.com>
2017-03-01 19:43:19 +0200

    ticket:1 new migration

--- apps/hello/migrations/0002_load_initial_data.py
+++ apps/hello/migrations/0002_load_initial_data.py
@@ -8,25 +8,11 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Bio'
-        db.create_table(u'hello_bio', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=30)),
-            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30)),
-            ('date_of_birth', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
-            ('bio', self.gf('django.db.models.fields.TextField')(max_length=300, null=True, blank=True)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=30, null=True, blank=True)),
-            ('jabber', self.gf('django.db.models.fields.CharField')(max_length=30, null=True, blank=True)),
-            ('skype', self.gf('django.db.models.fields.CharField')(max_length=30, null=True, blank=True)),
-            ('other_contacts', self.gf('django.db.models.fields.TextField')(max_length=300, null=True, blank=True)),
-        ))
-        
-        db.send_create_signal(u'hello', ['Bio'])
-
+        from django.core.management import call_command
+        call_command('loaddata', 'initial_data.json')
 
     def backwards(self, orm):
-        # Deleting model 'Bio'
-        db.delete_table(u'hello_bio')
+        "Write your backwards methods here."
 
 
     models = {
