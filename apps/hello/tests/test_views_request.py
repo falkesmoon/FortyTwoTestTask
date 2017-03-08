a4e2bd4e14233f48bf972ecba6dd41c76d6a6cfa
falkesmoon <falkesmoon@gmail.com>
2017-03-08 18:23:16 +0200

    ticket:1 test for middleware

--- apps/hello/tests/test_views_request.py
+++ apps/hello/tests/test_views_request.py
@@ -7,7 +7,6 @@ from django.utils import timezone
 class TestViewRequests(TestCase):
     def test_view_requests(self):
         """ testing request view render correctly"""
-        self.client = Client()
         self.url = reverse('request_list')
         response = self.client.get(self.url)
         self.assertTemplateUsed(response, 'requests.html')
