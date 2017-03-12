2a389a4439419a82555b2e7e4902be461bfa7f4a
falkesmoon <falkesmoon@gmail.com>
2017-03-12 15:15:20 +0200

    new db created

--- apps/hello/migrations/0002_load_initial_data.py
+++ apps/hello/migrations/0002_load_initial_data.py
@@ -9,7 +9,7 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         from django.core.management import call_command
-        call_command('flush', '--no-input')
+        
         call_command('loaddata', 'initial_data.json')
         
 
